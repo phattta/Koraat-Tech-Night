2969392d92753acb2b22df792ad9ecdcf114c267
phattta <pha.ttta2021@gmail.com>
2025-09-16 15:51:38 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex text-3xl justify-center items-baseline">
+      KORAT TECH NIGHT
+    </div>
+  )
+}
